1d326376acff6c883c05e0b94b38e69a96a8740f
hazamashoken <earth78203@gmail.com>
2024-07-30 23:05:47 +0800

    remove debuger

--- src/com/tliangso/Baloon.java
+++ src/com/tliangso/Baloon.java
@@ -16,30 +16,27 @@ public class Baloon extends Aircraft {
 
         switch (weather) {
 
-        case Constants.WEATHER_SUN:
-            System.out.printf(Constants.MESSAGE_SUN, "Baloon", this.name, this.id);
-            this.coordinates = new Coordinates(longitude + 2, latitude, height + 4);
-            break;
+            case Constants.WEATHER_SUN:
+                System.out.printf(Constants.MESSAGE_SUN, "Baloon", this.name, this.id);
+                this.coordinates = new Coordinates(longitude + 2, latitude, height + 4);
+                break;
 
-        case Constants.WEATHER_RAIN:
-            System.out.printf(Constants.MESSAGE_RAIN, "Baloon", this.name, this.id);
-            this.coordinates = new Coordinates(longitude, latitude, height - 5);
-            break;
+            case Constants.WEATHER_RAIN:
+                System.out.printf(Constants.MESSAGE_RAIN, "Baloon", this.name, this.id);
+                this.coordinates = new Coordinates(longitude, latitude, height - 5);
+                break;
 
-        case Constants.WEATHER_FOG:
-            System.out.printf(Constants.MESSAGE_FOG, "Baloon", this.name, this.id);
-            this.coordinates = new Coordinates(longitude, latitude, height - 3);
-            break;
+            case Constants.WEATHER_FOG:
+                System.out.printf(Constants.MESSAGE_FOG, "Baloon", this.name, this.id);
+                this.coordinates = new Coordinates(longitude, latitude, height - 3);
+                break;
 
-        case Constants.WEATHER_SNOW:
-            System.out.printf(Constants.MESSAGE_SNOW, "Baloon", this.name, this.id);
-            this.coordinates = new Coordinates(longitude, latitude, height - 15);
-            break;
+            case Constants.WEATHER_SNOW:
+                System.out.printf(Constants.MESSAGE_SNOW, "Baloon", this.name, this.id);
+                this.coordinates = new Coordinates(longitude, latitude, height - 15);
+                break;
         }
         if (this.coordinates.getHeight() <= 0) {
-            System.out.printf("%d %d %d\n", this.coordinates.getLongitude(), this.coordinates.getLatitude(),
-                    this.coordinates.getHeight());
-
             System.out.printf(Constants.MESSAGE_LAND, "Baloon", this.name, this.id);
             this.weatherTower.unregister(this);
             System.out.printf(Constants.MESSAGE_UNREGISTER, "Baloon", this.name, this.id);
--- src/com/tliangso/JetPlane.java
+++ src/com/tliangso/JetPlane.java
@@ -15,31 +15,28 @@ public class JetPlane extends Aircraft {
         Integer height = this.coordinates.getHeight();
 
         switch (weather) {
-        case Constants.WEATHER_SUN:
-            System.out.printf(Constants.MESSAGE_SUN, "JetPlane", this.name, this.id);
-            this.coordinates = new Coordinates(longitude, latitude + 10, height + 2);
-            break;
+            case Constants.WEATHER_SUN:
+                System.out.printf(Constants.MESSAGE_SUN, "JetPlane", this.name, this.id);
+                this.coordinates = new Coordinates(longitude, latitude + 10, height + 2);
+                break;
 
-        case Constants.WEATHER_RAIN:
-            System.out.printf(Constants.MESSAGE_RAIN, "JetPlane", this.name, this.id);
-            this.coordinates = new Coordinates(longitude, latitude + 5, height);
-            break;
+            case Constants.WEATHER_RAIN:
+                System.out.printf(Constants.MESSAGE_RAIN, "JetPlane", this.name, this.id);
+                this.coordinates = new Coordinates(longitude, latitude + 5, height);
+                break;
 
-        case Constants.WEATHER_FOG:
-            System.out.printf(Constants.MESSAGE_FOG, "JetPlane", this.name, this.id);
-            this.coordinates = new Coordinates(longitude, latitude + 1, height);
-            break;
+            case Constants.WEATHER_FOG:
+                System.out.printf(Constants.MESSAGE_FOG, "JetPlane", this.name, this.id);
+                this.coordinates = new Coordinates(longitude, latitude + 1, height);
+                break;
 
-        case Constants.WEATHER_SNOW:
-            System.out.printf(Constants.MESSAGE_SNOW, "JetPlane", this.name, this.id);
-            this.coordinates = new Coordinates(longitude, latitude, height - 7);
-            break;
+            case Constants.WEATHER_SNOW:
+                System.out.printf(Constants.MESSAGE_SNOW, "JetPlane", this.name, this.id);
+                this.coordinates = new Coordinates(longitude, latitude, height - 7);
+                break;
 
         }
         if (this.coordinates.getHeight() <= 0) {
-            System.out.printf("%d %d %d\n", this.coordinates.getLongitude(), this.coordinates.getLatitude(),
-                    this.coordinates.getHeight());
-
             System.out.printf(Constants.MESSAGE_LAND, "JetPlane", this.name, this.id);
             this.weatherTower.unregister(this);
             System.out.printf(Constants.MESSAGE_UNREGISTER, "JetPlane", this.name, this.id);
